cb37ed3b0e0cf09f9656932aa52c3efee1294246
Noel Flores <noel.flores@ooqia.com>
2019-06-10 16:19:07 -0600

    added shortcodes to show career and contracts on homepage

---
+++ themes/divi-child/includes/shortcodes.php
@@ -0,0 +1,29 @@
+<?php
+
+
+  // Shortcodes
+  function recent_contracts_function() {
+    global $post;
+      $html = "";
+      $my_query = new WP_Query( array(
+           'post_type' => 'contracts',
+           'posts_per_page' => 3
+      ));
+      if( $my_query->have_posts() ) : while( $my_query->have_posts() ) : $my_query->the_post();
+          $html .= "<div class='custom-post-type-list'>";
+            $html .= "<h3 class='title'>" . get_the_title() . " </h3>";
+            $html .= "<div class='date'>" . get_field('date_of_contract') . " </div>";
+            $html .= "<div class='content'>";
+              $html .= "<div class='contract-image'>" .get_the_post_thumbnail() . "</div>";
+              $html .= "<div class='contract-description'>" .get_field('brief_description') . "</div>";
+            $html .= "</div>";
+          $html .= "</div>";
+         endwhile;
+       wp_reset_postdata();
+   endif;
+      return $html;
+  }
+  function register_shortcodes(){
+     add_shortcode('ContractList', 'recent_contracts_function');
+  }
+  add_action( 'init', 'register_shortcodes');
