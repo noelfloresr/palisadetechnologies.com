82effd15f4a5a464929ca7e09856d80672c04322
Noel Flores <noel.flores@ooqia.com>
2019-06-14 04:58:56 -0600

    fixing mobile icons

--- themes/divi-child/src/js/index.js
+++ themes/divi-child/src/js/index.js
@@ -1,15 +1,33 @@
 jQuery(document).ready(function($){
   $(".mobile_menu_bar").on("click", function(){
-    console.log("clicked");
+
     var mobile_nav = $('.mobile_nav');
-    console.log(mobile_nav.hasClass("opened"));
+
+    
+
     if (mobile_nav.hasClass("opened")) {
       $('.et_menu_container').removeClass('opened');
-      $('#logo').attr('src', '/wp-content/uploads/logo.png')
+      $('#logo').attr('src', '/wp-content/uploads/logo.png');
     } else {
       $('.et_menu_container').addClass('opened');
-      $('#logo').attr('src', '/wp-content/uploads/logo-hover.png')
+      $('#logo').attr('src', '/wp-content/uploads/logo-hover.png');
+         
     }
-    
  });
+
+    // var infoSelector = $('ul#mobile_menu > #menu-item-28 > a');
+    // var careerSelector = $('ul#mobile_menu #menu-item-33 > a');
+    // var contactSelector = $('ul#mobile_menu #menu-item-36 > a');
+    // console.log(infoSelector);
+
+    // var infoHtml = infoSelector.html();
+    // var careerHtml = careerSelector.html();
+    // var contactHtml = contactSelector.html();
+
+    // // Adding Menu Icons
+    //   infoSelector.html('<img src="/wp-content/uploads/info.png" class="iconMenu" />' + infoHtml);        
+    //   careerSelector.html('<img src="/wp-content/uploads/briefcase.png" class="iconMenu" />' + careerHtml); 
+    //   contactSelector.html('<img src="/wp-content/uploads/phone-book.png" class="iconMenu" />' + contactHtml);
+    
+
 });
